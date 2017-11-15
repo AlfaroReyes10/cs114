bead811c473a95e1471476d7692db9a68186308b
s203 <s203@2024-01-203-15.local>
2017-11-15 10:51:58 -0800

    updates

---
+++ exercise.py
@@ -0,0 +1,15 @@
+import random
+print('Welcome To The Character Counting Program')
+print('Please type out your name and you will recieve a number')
+input()
+answer = Random_word
+def getanswer(Random_word):
+    if Random_word == 4:
+        return '4'
+    elif Random_word == 6:
+        return '6'
+    elif Random_word == 5:
+        return ' 7'
+r = random.randint(1,9)
+number = getanswer(r)
+print(answer)
---
+++ magic8ball-func.py
@@ -0,0 +1,37 @@
+import random
+
+print("This is a magic 8 ball program...")
+print("Welcome what is your name? Afterwards please press enter to learn your fate.")
+input()
+
+def get_random():
+    random_num = random.randint(1, 9)
+    return random_num
+
+def Fortune_Teller(random_num):
+
+
+    if   random_num == 1:
+        print("Today you will die")
+    elif random_num == 2:
+        print("Today you will recieve a special surprise")
+    elif random_num == 3:
+        print("You will win the lottery")
+    elif random_num == 4:
+        print("You will fall off a cliff one day")
+    elif random_num == 5:
+        print("You will recieve good news")
+    elif random_num == 6:
+        print("YOU'VE WON")
+    elif random_num == 7:
+        print("TRY AGAIN")
+    elif random_num == 8:
+        print("Have Faith")
+    elif random_num == 9:
+        print("You will find the love of your life today")
+
+def main():
+    random_num = get_random()
+    output = Fortune_Teller(random_num)
+    print(output)
+main()
